3f15daa26aa87fbb74f28282a201e71e1535c791
WADBB <kiki8.fs@gmail.com>
2026-01-25 17:42:28 +0100

    Refactor SingleValueDecodingContainer methods

--- Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
+++ Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
@@ -1,58 +1,30 @@
 import Foundation
 
-// MARK: - SingleValueDecodingContainer conformance (solo il minimo indispensabile)
+// Solo la conformità + metodi richiesti
 extension XMLDecoderImplementation: SingleValueDecodingContainer {
     public func decodeNil() -> Bool {
-        return (try? topContainer().isNull) ?? true
+        (try? topContainer().isNull) ?? true
     }
 
-    public func decode<T: Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
+    public func decode(_: Bool.Type) throws -> Bool { try unbox(try topContainer()) }
+
+    public func decode<T: BinaryInteger & SignedInteger & Decodable>(_: T.Type) throws -> T {
+        try unbox(try topContainer())
     }
+    public func decode<T: BinaryInteger & UnsignedInteger & Decodable>(_: T.Type) throws -> T {
+        try unbox(try topContainer())
+    }
+
+    public func decode(_: Float.Type) throws -> Float { try unbox(try topContainer()) }
+    public func decode(_: Double.Type) throws -> Double { try unbox(try topContainer()) }
+    public func decode(_: String.Type) throws -> String { try unbox(try topContainer()) }
+
+    public func decode<T: Decodable>(_: T.Type) throws -> T { try unbox(try topContainer()) }
 }
 
-// MARK: - Estensione separata con tutte le implementazioni specifiche
+// Overload opzionali fuori dalla conformità → niente warning
 extension XMLDecoderImplementation {
-    // Questi metodi NON fanno parte della conformance al protocollo
-    // ma sono metodi di convenienza che puoi chiamare internamente
-    
-    public func decodeBool() throws -> Bool {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeDecimal() throws -> Decimal {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeSignedInteger<T: BinaryInteger & SignedInteger & Decodable>() throws -> T {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeUnsignedInteger<T: BinaryInteger & UnsignedInteger & Decodable>() throws -> T {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeFloat() throws -> Float {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeDouble() throws -> Double {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeString() throws -> String {
-        return try unbox(try topContainer())
-    }
-    
-    public func decodeDateFromString() throws -> Date {
-        let _: String = try decodeString()
-        // Logica di conversione
-        fatalError("Implement decodeDateFromString")
-    }
-    
-    public func decodeDataFromString() throws -> Data {
-        let _: String = try decodeString()
-        // Logica di conversione
-        fatalError("Implement decodeDataFromString")
-    }
+    public func decode(_: Date.Type) throws -> Date { try unbox(try topContainer()) }
+    public func decode(_: Data.Type) throws -> Data { try unbox(try topContainer()) }
+    public func decode(_: Decimal.Type) throws -> Decimal { try unbox(try topContainer()) }
 }
