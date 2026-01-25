49a01316b7cff73e7778bfc0598a965e36ce3e3b
WADBB <kiki8.fs@gmail.com>
2026-01-25 17:30:01 +0100

    Refactor XMLDecoderImplementation methods for clarity

--- Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
+++ Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
@@ -1,33 +1,9 @@
 import Foundation
 
-// MARK: - SingleValueDecodingContainer conformance (solo i metodi che NON danno warning)
+// MARK: - SingleValueDecodingContainer conformance (solo il minimo indispensabile)
 extension XMLDecoderImplementation: SingleValueDecodingContainer {
     public func decodeNil() -> Bool {
         return (try? topContainer().isNull) ?? true
-    }
-
-    public func decode(_: Bool.Type) throws -> Bool {
-        return try unbox(try topContainer())
-    }
-
-    public func decode<T: BinaryInteger & SignedInteger & Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
-    }
-
-    public func decode<T: BinaryInteger & UnsignedInteger & Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: Float.Type) throws -> Float {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: Double.Type) throws -> Double {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: String.Type) throws -> String {
-        return try unbox(try topContainer())
     }
 
     public func decode<T: Decodable>(_: T.Type) throws -> T {
@@ -35,24 +11,48 @@ extension XMLDecoderImplementation: SingleValueDecodingContainer {
     }
 }
 
-// MARK: - Estensione separata per i metodi che causano warning
-private extension XMLDecoderImplementation {
-    // Metodo per Decimal (causa warning se è nell'estensione del protocollo)
-    func decodeDecimal() throws -> Decimal {
+// MARK: - Estensione separata con tutte le implementazioni specifiche
+extension XMLDecoderImplementation {
+    // Questi metodi NON fanno parte della conformance al protocollo
+    // ma sono metodi di convenienza che puoi chiamare internamente
+    
+    public func decodeBool() throws -> Bool {
         return try unbox(try topContainer())
     }
     
-    // Metodo per Date (se necessario)
-    func decodeDateFromString() throws -> Date {
-        let stringValue: String = try self.decode(String.self)
-        // Implementa la tua logica di conversione
+    public func decodeDecimal() throws -> Decimal {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeSignedInteger<T: BinaryInteger & SignedInteger & Decodable>() throws -> T {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeUnsignedInteger<T: BinaryInteger & UnsignedInteger & Decodable>() throws -> T {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeFloat() throws -> Float {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeDouble() throws -> Double {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeString() throws -> String {
+        return try unbox(try topContainer())
+    }
+    
+    public func decodeDateFromString() throws -> Date {
+        let stringValue: String = try decodeString()
+        // Logica di conversione
         fatalError("Implement decodeDateFromString")
     }
     
-    // Metodo per Data (se necessario)
-    func decodeDataFromString() throws -> Data {
-        let stringValue: String = try self.decode(String.self)
-        // Implementa la tua logica di conversione
+    public func decodeDataFromString() throws -> Data {
+        let stringValue: String = try decodeString()
+        // Logica di conversione
         fatalError("Implement decodeDataFromString")
     }
 }
