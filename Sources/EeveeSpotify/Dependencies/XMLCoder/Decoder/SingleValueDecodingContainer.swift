dde20913aaf0db9f001fcb6db89a698fd0447ba3
WADBB <kiki8.fs@gmail.com>
2026-03-11 01:56:49 +0100

    Update SingleValueDecodingContainer.swift

--- Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
+++ Sources/EeveeSpotify/Dependencies/XMLCoder/Decoder/SingleValueDecodingContainer.swift
@@ -1,57 +1,30 @@
-// Copyright (c) 2017-2020 Shawn Moore and XMLCoder contributors
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-//
-//  Created by Shawn Moore on 11/20/17.
-//
-
 import Foundation
 
+// Solo la conformità + metodi richiesti
 extension XMLDecoderImplementation: SingleValueDecodingContainer {
-    // MARK: SingleValueDecodingContainer Methods
-
     public func decodeNil() -> Bool {
-        return (try? topContainer().isNull) ?? true
+        (try? topContainer().isNull) ?? true
     }
 
-    public func decode(_: Bool.Type) throws -> Bool {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: Decimal.Type) throws -> Decimal {
-        return try unbox(try topContainer())
-    }
+    public func decode(_: Bool.Type) throws -> Bool { try unbox(try topContainer()) }
 
     public func decode<T: BinaryInteger & SignedInteger & Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
+        try unbox(try topContainer())
     }
-
     public func decode<T: BinaryInteger & UnsignedInteger & Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
+        try unbox(try topContainer())
     }
 
-    public func decode(_: Float.Type) throws -> Float {
-        return try unbox(try topContainer())
-    }
+    public func decode(_: Float.Type) throws -> Float { try unbox(try topContainer()) }
+    public func decode(_: Double.Type) throws -> Double { try unbox(try topContainer()) }
+    public func decode(_: String.Type) throws -> String { try unbox(try topContainer()) }
 
-    public func decode(_: Double.Type) throws -> Double {
-        return try unbox(try topContainer())
-    }
+    public func decode<T: Decodable>(_: T.Type) throws -> T { try unbox(try topContainer()) }
+}
 
-    public func decode(_: String.Type) throws -> String {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: String.Type) throws -> Date {
-        return try unbox(try topContainer())
-    }
-
-    public func decode(_: String.Type) throws -> Data {
-        return try unbox(try topContainer())
-    }
-
-    public func decode<T: Decodable>(_: T.Type) throws -> T {
-        return try unbox(try topContainer())
-    }
+// Overload opzionali fuori dalla conformità → niente warning
+extension XMLDecoderImplementation {
+    public func decode(_: Date.Type) throws -> Date { try unbox(try topContainer()) }
+    public func decode(_: Data.Type) throws -> Data { try unbox(try topContainer()) }
+    public func decode(_: Decimal.Type) throws -> Decimal { try unbox(try topContainer()) }
 }
